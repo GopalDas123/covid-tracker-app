1ea4b1165414c84fd25569d08d65ee77839525c3
GopalDas123 <Gopaldax@gmail.com>
2020-09-06 00:13:45 +0500

    Making Some Changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from "react";
 import "./App.css";
-import Gri from "./components/Grids";
-import SearchAppBar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      {/* <h1>Hello From Gopal</h1> */}
-      <SearchAppBar />
-      <Gri />
+      <h1>Hello There</h1>
     </div>
   );
 }
